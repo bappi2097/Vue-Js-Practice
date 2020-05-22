8a3bcbbc7477b4f071e327594e8dee7e4d71784a
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-05-23 00:16:19 +0600

    Lesson 11 continue

--- 11-Practical-Component-Exercise-3-Tabs/main.js
+++ 11-Practical-Component-Exercise-3-Tabs/main.js
@@ -1,3 +1,37 @@
+// @ts-nocheck
+Vue.component("tabs", {
+  template: `
+    <div>
+      <div class="tabs">
+        <ul>
+          <li class="is-active"><a>Pictures</a></li>
+          <li><a>Music</a></li>
+          <li><a>Videos</a></li>
+          <li><a>Documents</a></li>
+        </ul>
+      </div>
+      <div class="tabs-details">
+        <slot></slot>
+      </div>
+    </div>
+  `,
+  data() {
+    return { tabs: [] };
+  },
+  created() {
+    this.tabs = this.$children;
+  },
+});
+
+Vue.component("tab", {
+  template: `
+    <div><slot></slot></div>
+  `,
+  props: {
+    name: { required: true },
+  },
+});
+
 new Vue({
   el: "#root",
 });
