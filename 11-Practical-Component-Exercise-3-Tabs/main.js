b31f4cf4a282665bdb0ffeda0a076bece3de519b
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-05-23 17:40:15 +0600

    lesson 11 done

--- 11-Practical-Component-Exercise-3-Tabs/main.js
+++ 11-Practical-Component-Exercise-3-Tabs/main.js
@@ -1,37 +1,56 @@
-// @ts-nocheck
+// @ts-ignore
 Vue.component("tabs", {
   template: `
-    <div>
-      <div class="tabs">
-        <ul>
-          <li class="is-active"><a>Pictures</a></li>
-          <li><a>Music</a></li>
-          <li><a>Videos</a></li>
-          <li><a>Documents</a></li>
-        </ul>
-      </div>
-      <div class="tabs-details">
-        <slot></slot>
-      </div>
+  <div>
+    <div class="tabs">
+      <ul>
+        <li v-for="tab in tabs" :class="{'is-active': tab.isActive}">
+          <a href="#" @click="selectTab(tab)">
+            {{tab.name}}
+          </a>
+        </li>
+      </ul>
     </div>
+    <div class="tabs-details"><slot></slot></div>
+  </div>
   `,
   data() {
-    return { tabs: [] };
+    return {
+      tabs: [],
+    };
+  },
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach((tab) => {
+        tab.isActive = tab.name == selectedTab.name;
+      });
+    },
   },
   created() {
     this.tabs = this.$children;
   },
 });
 
+// @ts-ignore
 Vue.component("tab", {
   template: `
-    <div><slot></slot></div>
+  <div v-show="isActive"><slot></slot></div>
   `,
   props: {
     name: { required: true },
+    selected: { default: false },
+  },
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  mounted() {
+    this.isActive = this.selected;
   },
 });
 
+// @ts-ignore
 new Vue({
   el: "#root",
 });
