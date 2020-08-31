7b8ecf837df2731fd2350428b0db28e7c96051fd
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-08-31 15:51:37 +0600

    inter communication complete

--- js/app.js
+++ js/app.js
@@ -1,54 +1,21 @@
-Vue.component("tabs", {
+Vue.component("cuppon", {
   template: `
-  <div>
-    <div class="tabs">
-      <ul>
-        <li v-for="tab in tabs" :class="{'is-active':tab.isActive}">
-          <a :href="tab.href" @click="selectTab(tab)">{{tab.name}}</a>
-        </li>
-      </ul>
-    </div>
-    <div class="tabs-detail"><slot></slot></div>
-  </div>
+  <input @change="cupponApplied" type="text">
   `,
-  data() {
-    return {
-      tabs: [],
-    };
-  },
-  created() {
-    this.tabs = this.$children;
-  },
   methods: {
-    selectTab(selectedTab) {
-      this.tabs.forEach((tab) => {
-        tab.isActive = tab.name == selectedTab.name;
-      });
-    },
-  },
-});
-Vue.component("tab", {
-  props: {
-    name: { required: true },
-    selected: { default: false },
-  },
-  template: `
-  <div v-show="isActive"><slot></slot></div>
-  `,
-  data() {
-    return {
-      isActive: false,
-    };
-  },
-  mounted() {
-    this.isActive = this.selected;
-  },
-  computed: {
-    href() {
-      return "#" + this.name.toLowerCase().replace(/ /gi, "-");
+    cupponApplied() {
+      this.$emit("applied");
     },
   },
 });
 var root = new Vue({
   el: "#root",
+  methods: {
+    onCupponApplied() {
+      this.isApplied = true;
+    },
+  },
+  data: {
+    isApplied: false,
+  },
 });
