a2d93994fa939491693d71e92073c3c942cbde60
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-09-04 00:09:36 +0600

    lesson 17 complete

---
+++ lesson/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+export default {
+  name: "app",
+  components: { Counter },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ lesson/src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>This is : {{count}}</p>
+    <button @click="count+=10">Increment</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
