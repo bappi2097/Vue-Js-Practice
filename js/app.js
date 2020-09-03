8eaa7368ff9f48d58123bfeae15f0ea155b795c6
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-09-03 21:48:54 +0600

    lesson 16 complete

--- js/app.js
+++ js/app.js
@@ -1,39 +1,8 @@
-Vue.component("modal", {
-  template: `
-    <div class="modal" :class="{'is-active' : isActive}">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">
-          <slot name="header"></slot>
-          </p>
-          <button class="delete" aria-label="close" @click="isActive=false"></button>
-        </header>
-        <section class="modal-card-body">
-          <slot>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, impedit
-          asperiores! Corporis illum qui odit ducimus modi et, saepe doloribus cumque
-          adipisci molestiae exercitationem dicta ut debitis quis laborum impedit!
-          </slot>
-        </section>
-        <footer class="modal-card-foot">
-        <slot name="footer">
-        <button class="button is-success">Okay</button>
-        </slot>
-        </footer>
-      </div>
-    </div>
-  `,
-  props: {
-    active: { default: false },
-  },
+Vue.component("progress-view", {
   data() {
     return {
-      isActive: false,
+      completeRate: 0,
     };
-  },
-  created() {
-    this.isActive = this.active;
   },
 });
 var root = new Vue({
---
+++ zpp/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <message>Hello World</message>
+    <message>Hello World</message>
+    <message>Hello World</message>
+  </div>
+</template>
+
+<script>
+import Message from "./components/Message.vue";
+export default {
+  name: "app",
+  components: { Message },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
