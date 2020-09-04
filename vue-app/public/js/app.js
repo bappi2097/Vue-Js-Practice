2ac826a86f322f1197ad8926b51a6787e39867a7
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-09-04 19:49:19 +0600

    lesson 20 complete

--- vue-app/public/js/app.js
+++ vue-app/public/js/app.js
@@ -2,49 +2,98 @@ class Errors {
     constructor() {
         this.errors = {};
     }
+
     any() {
         return Object.keys(this.errors).length > 0;
     }
+
     clear(field) {
-        delete this.errors[field];
+        if (field) {
+            delete this.errors[field];
+            return;
+        }
+        this.errors = {};
     }
+
     get(field) {
         if (this.errors[field]) {
             return this.errors[field][0];
         }
     }
+
     has(field) {
         return this.errors.hasOwnProperty(field);
     }
+
     record(data) {
         this.errors = data;
     }
 }
+
+class Form {
+    constructor(data) {
+        this.originalData = data;
+        for (let field in data) {
+            this[field] = data[field];
+        }
+        this.errors = new Errors();
+    }
+
+    data() {
+        //  data {name:'bappi', description:'lorem psum amet'}
+        let data = Object.assign({}, this);
+        delete data.originalData;
+        delete data.errors;
+        return data;
+    }
+
+    get(url) {
+        //
+    }
+
+    onFail(error) {
+        //
+        this.errors.record(error.response.data.errors);
+    }
+
+    onSuccess(response) {
+        //
+        alert(response.data.message);
+    }
+
+    post(url) {
+        this.submit("post", url);
+    }
+
+    reset() {
+        //
+        for (let field in this.originalData) {
+            this[field] = "";
+        }
+    }
+
+    submit(requestType, url) {
+        //  axios
+        axios[requestType](url, this.data())
+            .then(this.onSuccess.bind(this))
+            .catch(this.onFail.bind(this));
+    }
+}
+
 var root = new Vue({
     el: "#root",
     data: {
-        name: "",
-        description: "",
+        form: new Form({
+            name: "",
+            description: "",
+        }),
         projects: [],
-        errors: new Errors(),
     },
     methods: {
         onSubmit() {
-            axios
-                .post("/create", {
-                    name: this.name,
-                    description: this.description,
-                })
-                .then(this.onSuccess)
-                .catch((error) =>
-                    this.errors.record(error.response.data.errors)
-                );
-        },
-        onSuccess(response) {
-            this.name = "";
-            this.description = "";
-            alert(response.data.message);
+            this.form.post("/create");
             this.loadData();
+            this.form.reset();
         },
         loadData() {
             axios
--- vue-app/resources/views/projects/create.blade.php
+++ vue-app/resources/views/projects/create.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lesson 19</title>
+    <title>Lesson 20</title>
     <link rel="stylesheet" href="{{asset('css/bulma.min.css')}}">
     <script src="{{asset('js/vue.js')}}"></script>
     <script src="{{asset('js/axios.min.js')}}"></script>
@@ -17,22 +17,22 @@
         <div id="root">
             @include('projects.list')
             <form action="{{route('create')}}" method="post" class="form" @submit.prevent="onSubmit"
-                @keypress="errors.clear($event.target.name)">
+                @keypress="form.errors.clear($event.target.name)">
                 <br>
                 <div class="control">
                     <label for="name" class="label">Project Name</label>
-                    <input type="text" name="name" id="name" class="input is-primary" v-model="name">
-                    <span class="help is-danger" v-text="errors.get('name')" v-if="errors.has('name')"></span>
+                    <input type="text" name="name" id="name" class="input is-primary" v-model="form.name">
+                    <span class="help is-danger" v-text="form.errors.get('name')" v-if="form.errors.has('name')"></span>
                 </div><br>
                 <div class="control">
                     <label for="description" class="label">Project Description</label>
                     <input type="text" name="description" id="description" class="input is-primary"
-                        v-model="description">
-                    <span class="help is-danger" v-text="errors.get('description')"
-                        v-if="errors.has('description')"></span>
+                        v-model="form.description">
+                    <span class="help is-danger" v-text="form.errors.get('description')"
+                        v-if="form.errors.has('description')"></span>
                 </div><br>
                 <div class="control">
-                    <button class="button is-primary" :disabled="errors.any()">Save</button>
+                    <button class="button is-primary" :disabled="form.errors.any()">Save</button>
                 </div>
             </form>
         </div>
