a8efedaf8f647ecc973ba0e3f574021ff68dde7f
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-09-05 19:40:54 +0600

    lesson 24 complete

--- 23-Laravel-Mix/resources/views/welcome.blade.php
+++ 23-Laravel-Mix/resources/views/welcome.blade.php
@@ -5,18 +5,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Lesson 24</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <!-- Styles -->
+    <script src="{{asset('js/vue.js')}}"></script>
+    <script src="{{asset('js/axios.min.js')}}"></script>
 
 </head>
 
 <body>
-    <div id="root">
-        <notification></notification>
+    <div id="one">
+        <h1>@{{shared.user.name}}</h1>
+    </div>
+    <div id="two">
+        <h1>@{{shared.user.name}}</h1>
     </div>
     <script src="{{asset('js/app.js')}}"></script>
 </body>
