b9ec28bda30ecc0709ce4e25e0634bcbc74165e5
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-09-06 19:10:35 +0600

    stream added

---
+++ stream/resources/js/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Home Page</div>
+                    <div class="panel-body">
+                        This is Home Page.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
---
+++ stream/resources/views/layouts/nav.blade.php
@@ -0,0 +1,15 @@
+<nav class="tabs is-boxed is-centered">
+    <div class="container">
+        <ul>
+            <router-link to="/" tag="li" exact>
+                <a>Home</a>
+            </router-link>
+            <router-link to="/about" tag="li">
+                <a>About</a>
+            </router-link>
+            <router-link to="/contact" tag="li">
+                <a>Contact</a>
+            </router-link>
+        </ul>
+    </div>
+</nav>
