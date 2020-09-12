f1e5f053c2537a87b224a66ee8422ec88a813e6b
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-09-12 23:30:59 +0600

    lesson 33 complete

--- 33-Scoped-SLots/resources/js/app.js
+++ 33-Scoped-SLots/resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import Vue from "vue";
+import Example from "./components/Example.vue";
+window.Vue = Vue;
+
+Vue.component("my-items", Example);
+
+var root = new Vue({
+    el: "#root"
+});
---
+++ 33-Scoped-SLots/resources/js/components/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items">
+        <slot :item="item" name="my-items">{{item}}</slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+};
+</script>
